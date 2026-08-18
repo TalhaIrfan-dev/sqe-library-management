735b7e5696a51118f18aed243c75c42f46bd433f
TalhaIrfan <talhairfanchoudry@gmail.com>
2026-08-19 00:37:15 +0500

    feat(library): add book creation

--- src/main.java
+++ src/main.java
@@ -1,4 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class main
 {
-    
+    static List<Book> books = new ArrayList<>();
+
+    static void addBook(String title, String author, int copies)
+    {
+        Book book = new Book(title, author, copies);
+        books.add(book);
+    }
 }
